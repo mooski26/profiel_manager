012729f247508f9dd146200670c211ed8c2a91a5
mooski26 <yuanfikri2604@gmail.com>
2021-02-01 20:38:32 +0700

    profil commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\UserController;
+
+Auth::routes();
+
+Route::get('/halo', [App\Http\Controllers\HomeController::class, 'index'])
+->name('home');
+
+Route::get('/users/{user}/edit', [UserController::class,'edit'])
+->middleware('can:update,user');
+
+Route::patch('/users/{user}', [UserController::class,'update'])
+->middleware('can:update,user');
+
+Route::delete('/users/{user}', [UserController::class,'destroy'])
+->middleware('can:delete,user');
